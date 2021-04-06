0a5cf58fafe9e0d13a51d26ef05b7bb8b209273a
Omar Magdy <contact.parmag@gmail.com>
2021-04-06 21:22:06 +0200

    pydantic_models.py:add:InputsOfEncryptionEndpoint

---
+++ pydantic_models.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, constr
+
+
+passphrase_constraint = constr(min_length=2, max_length=1000000)
+message_constraint = constr(max_length=100000000000)
+
+
+class InputsOfEncryptionEndpoint(BaseModel):
+    passphrase = passphrase_constraint
+    message = message_constraint

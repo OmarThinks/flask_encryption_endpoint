f632a21fd6428c09ade41652022f77f8bc0e0b54
Omar Magdy <contact.parmag@gmail.com>
2021-04-06 21:25:52 +0200

    test_pydantic_models.py:created

--- pydantic_models.py
+++ pydantic_models.py
@@ -5,6 +5,6 @@ passphrase_constraint = constr(min_length=2, max_length=1000000)
 message_constraint = constr(max_length=100000000000)
 
 
-class InputsOfEncryptionEndpoint(BaseModel):
+class EncryptionInputs(BaseModel):
     passphrase = passphrase_constraint
     message = message_constraint

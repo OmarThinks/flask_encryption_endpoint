a698f7a17feb59c060517ded51cf8e654df37d53
Omar Magdy <contact.parmag@gmail.com>
2021-04-07 13:01:16 +0200

    cryp.py:add:encrpy_gpg function

--- cryp.py
+++ cryp.py
@@ -3,8 +3,18 @@ import gnupg
 
 
 
-def encrypt_gpg(gpg, data, recipients = None):
-	return gpg.encrypt(data, recipients)
+def encrypt_gpg(gpg, original:str, passphrase:str):
+	gpg = gnupg.GPG()
+	original = "123"
+	gpg.encoding = 'utf-8'
+	encrypted_data = gpg.encrypt(original, 
+		passphrase = passphrase, recipients=None, 
+		symmetric = True)
+	return {
+	"status": encrypted_data.status,
+	"data": str(encrypted_data.data,"utf-8"),
+	"success":encrypted_data.success
+	}	
 
 def decrypt_gpg(gpg, message, passphrase):
 	return gpg.decrypt(message, passphrase = passphrase,

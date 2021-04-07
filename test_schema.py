05b38250d5226e4ec359bf4be0523895e343f356
Omar Magdy <contact.parmag@gmail.com>
2021-04-07 21:50:54 +0200

    test_schema.py:add:prepare decryption

--- test_schema.py
+++ test_schema.py
@@ -45,7 +45,7 @@ query{
 		#print((result.data))
 		self.assertEqual(type(
 			result.data["encryptMessage"]),str)
-		print("test_001: successful")
+		print("test_001: Encryption successful")
 
 
 	def test_002_missing_inputs(self):
@@ -63,7 +63,74 @@ found )\n\n1: query{encryptMessage()}\n                        ^\n',
 			raise Exception("result does not have data")
 		except Exception as e:
 			pass
-		print("test_002_missing_inputs")
+		print("test_002_Encryprion_missing_inputs")
+
+
+
+
+
+
+
+class Schema_2_DecryptionTestCase(unittest.TestCase):
+	
+	def tearDown(self):
+		"""Executed after reach test"""
+		print("_+++++++++++++++++++++++++++++++++_")	
+
+	def test_000(self):
+		print("Strating Encryption Tests")
+		print("test_000: Hello, tests!")
+
+	
+	def test_001_successful_inputs(self):
+		variables = {"original": "123","passphrase":"secret"}
+		query = """
+query{
+	encryptMessage(
+		original:\""""+variables["original"]+"""\",
+		passphrase:\""""+variables["passphrase"]+"""\",
+	)
+}
+		"""
+		#print(query)
+		"""
+query{
+        encryptMessage(
+                original:"123",
+                passphrase:"secret",
+        )
+}
+"""
+		result = schema.execute(query)
+		#print(result)
+		"""
+{'data': {'encryptMessage': 
+'-----BEGIN PGP MESSAGE-----\n\njA0EBwMCnfx01GLicp7n0jgBK25j4EwOm+rM85AVyJv6p1uuhY1wY8oNP5dLKEB0\nvs7PW5hz2KM+D3+eBa/LXZI90MmCQEQQQA==\n=O6rp\n-----END PGP MESSAGE-----\n'
+}}
+		"""
+		#print(type(result.data))
+		#print((result.data))
+		self.assertEqual(type(
+			result.data["encryptMessage"]),str)
+		print("test_001: Decryption successful")
+
+
+	def test_002_missing_inputs(self):
+		query = "query{encryptMessage()}"
+
+		result = schema.execute(query)
+		#print(result)
+		"""
+{'errors': [{'message': 'Syntax Error GraphQL (1:22) Expected Name, 
+found )\n\n1: query{encryptMessage()}\n                        ^\n', 
+'locations': [{'line': 1, 'column': 22}]}]}
+		"""
+		try:
+			result.data
+			raise Exception("result does not have data")
+		except Exception as e:
+			pass
+		print("test_002_Decryprion_missing_inputs")
 
 
 

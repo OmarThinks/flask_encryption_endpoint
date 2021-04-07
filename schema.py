a20dc1d8118b80fb5c9f584a0458f34e4960a6be
Omar Magdy <contact.parmag@gmail.com>
2021-04-07 15:31:12 +0200

    schema.py:add:resolvers

--- schema.py
+++ schema.py
@@ -1,11 +1,48 @@
 import graphene
 
+from cryp import (encrypt_gpg, decrypt_gpg)
 
 class EncryptionResult(graphene.ObjectType):
-    encryptedMessage = graphene.String()
+    encrypted_message = graphene.String()
 
 class DecryptionResult(graphene.ObjectType):
-    decryptedMessage = graphene.String()
+    decrypted_message = graphene.String()
+
+
+class EncryptionError(Exception):
+	pass
+class DecryptionError(Exception):
+	pass
+
+
+class RootQuery(graphene.ObjectType):
+	encrypt_message = graphene.Field(EncryptionResult)
+	decrypt_message = graphene.Field(DecryptionResult)
+
+	def resolve_encrypt_message(root, info, 
+		original:str, passphrase:str):
+		encrypted = encrypt_gpg(original = original, 
+			passphrase = passphrase)
+		if encrypted["success"] == True:
+			return encrypted["data"]
+		else:
+			raise EncryptionError(encrypted["status"])
+
+	def resolve_decrypt_message(root, info, 
+		message:str, passphrase:str):
+		decrypted = encrypt_gpg(message = message, 
+			passphrase = passphrase)
+		if decrypted["success"] == True:
+			return decrypted["data"]
+		else:
+			raise DecryptionError(decrypted["status"])
+
+
+
+
+
+
+
 
 
 
